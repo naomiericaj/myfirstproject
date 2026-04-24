c1d7aebe9d079fc429b4083d473ec8503252c463
Naomi Erica <naomierica@Naomis-MacBook-Pro.local>
2026-04-24 09:50:41 +0700

    week 9 Laravel API and Payment Gateway

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -15,6 +15,12 @@
                 <li class="nav-item">
                     <a class="nav-link {{ request()->routeIs('about') ? 'active' : '' }}" href="{{ route('about') }}">About Us</a>
                 </li>
+
+                @auth
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('orders') ? 'active' : '' }}" href="{{ route('orders') }}">My Orders</a>
+                </li>
+                @endauth
             </ul>
             <div class="d-flex text-white align-items-center">
                 <a href="#" class="text-white me-3 fs-5"><i class="fab fa-facebook-f"></i></a>
@@ -24,6 +30,7 @@
                 @auth
                     <a href ="{{ route('view_cart') }}" class="text-white me-3 fs-5"> <i class="fas fa-shopping-cart"></i></a>
                 @endauth
+
                 
                 @guest
                     <a href="{{ route('login.show') }}" class="btn btn-light btn-sm ms-2 px-4 fw-bold rounded-pill text-primary shadow-sm hover-shadow">Login</a>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FirstController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\StoreController;
 use Illuminate\Routing\RouteUri;
 use Illuminate\Support\Facades\Route;
@@ -50,6 +51,10 @@ Route::middleware('auth')->group(function () {
          Route::patch('/update-cart/{product_id}', [StoreController::class, 'update_cart'])->name('update_cart');
          Route::delete('/remove-from-cart/{product_id}', [StoreController::class, 'remove_from_cart'])->name('remove_from_cart');
          Route::post('/checkout', [StoreController::class, 'checkout'])->name('checkout');
+         Route::get('/payment/return/{order_id}', [StoreController::class, 'payment_return'])->name('payment_return');
+         Route::get('/payment/status/({order_id})', [StoreController::class, 'payment_status'])->name('payment_status');
+         Route::get('/orders', [OrderController::class, 'index'])->name('orders');
+         Route::get('/order/{order_id}', [OrderController::class, 'order_details'])->name('order_details');
     });
 
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
